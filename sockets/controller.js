26de403b03bed3556eb4aeb340b6e6fa28fe7f47
Sergio Meza <svmezahernandez@gmail.com>
2022-04-16 22:51:21 -0500

    Primer Commit

---
+++ sockets/controller.js
@@ -0,0 +1,41 @@
+const { Socket } = require('socket.io');
+const { comprobarJWT } = require('../helpers');
+const { ChatMensajes } = require('../models');
+
+const chatMensajes = new ChatMensajes()
+
+const socketController = async (socket = new Socket(), io) => {
+
+    const usuario = await comprobarJWT(socket.handshake.headers['x-token']);
+    if (!usuario) {
+        return socket.disconnect();
+    }
+    chatMensajes.conectarUsuario(usuario);
+    io.emit('usuarios-activos', chatMensajes.usuariosArr);
+    socket.emit('usuarios-activos', chatMensajes.ultimos10);
+
+    socket.join(usuario.id);
+
+
+    socket.on('disconnect', () => {
+        chatMensajes.desconectarUsuario(usuario.id);
+        io.emit('usuarios-activos', chatMensajes.usuariosArr);
+    });
+
+    socket.on('enviar-mensaje', ({ uid, mensaje }) => {
+        if (uid) {
+            socket.to(uid).emit('mensaje-privado',{ de: usuario.nombre, mensaje });
+        } else {
+            chatMensajes.enviarMensaje(usuario.uid, usuario.nombre, mensaje);
+            io.emit('recibir-mensajes', chatMensajes.ultimos10);
+        }
+
+
+    });
+
+}
+
+
+module.exports = {
+    socketController
+}
